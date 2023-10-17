99f4b8c011e36ca61e24f619c27e5763d9bc761a
drithh <adrielalfeus@gmail.com>
2023-10-17 09:26:08 +0700

    revision

--- color-space-conversion/logic_operation.py
+++ color-space-conversion/logic_operation.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from rgb_grayscale_binary import convert_rgb_to_grayscale
 
 def operate_logical_operation(first_image, second_image, operator):
     # Ensure that both images have the same size
@@ -6,12 +7,15 @@ def operate_logical_operation(first_image, second_image, operator):
         raise ValueError("Images must have the same dimensions")
 
     # Create a new blank image with the same size
-    result_image = Image.new("RGB", first_image.size)
+    result_image = Image.new("1", first_image.size)
     # Check if the operator is valid
     operators = {
         'AND': logical_and,
         'OR': logical_or,
         'XOR': logical_xor,
+        'NAND': logical_nand,
+        'NOR': logical_nor,
+        'XNOR': logical_xnor
     }
 
     if operator not in operators:
@@ -19,14 +23,22 @@ def operate_logical_operation(first_image, second_image, operator):
 
     # Perform the specified logical operation
     operation_func = operators[operator]
+    
+    grayscale_first_image = convert_rgb_to_grayscale(first_image)
+    grayscale_second_image = convert_rgb_to_grayscale(second_image)
 
-    for x in range(first_image.width):
-        for y in range(first_image.height):
+    for x in range(grayscale_first_image.width):
+        for y in range(grayscale_second_image.height):
             # Get the pixel values at the current position
-            triangle_pixel = first_image.getpixel((x, y))
-            circle_pixel = second_image.getpixel((x, y))
+            triangle_pixel = grayscale_first_image.getpixel((x, y))
+            circle_pixel = grayscale_second_image.getpixel((x, y))
 
             # Perform the logical operation on the RGB channels
+            # convert to binary
+            triangle_pixel = 0 if triangle_pixel > 128 else 1
+            circle_pixel = 0 if circle_pixel else 1
+            
+            # perform the logical operation
             result_pixel = operation_func(triangle_pixel, circle_pixel)
 
             # Set the result pixel in the new image
@@ -36,13 +48,17 @@ def operate_logical_operation(first_image, second_image, operator):
 
 def operate_logical_not(image):
     # Create a new blank image with the same size
-    result_image = Image.new("RGB", image.size)
+    result_image = Image.new("1", image.size)
 
-    for x in range(image.width):
-        for y in range(image.height):
+    grayscale_image = convert_rgb_to_grayscale(image)
+    
+    for x in range(grayscale_image.width):
+        for y in range(grayscale_image.height):
             # Get the pixel values at the current position
-            pixel = image.getpixel((x, y))
+            pixel = grayscale_image.getpixel((x, y))
 
+            pixel = 0 if pixel > 128 else 1
+            
             # Perform the logical operation on the RGB channels
             result_pixel = logical_not(pixel)
 
@@ -53,13 +69,25 @@ def operate_logical_not(image):
 
 # Define functions for the logical operators
 def logical_and(x, y):
-    return (x[0] & y[0], x[1] & y[1], x[2] & y[2])
+    return 0 if (x & y == 1) else 1
 
 def logical_or(x, y):
-    return (x[0] | y[0], x[1] | y[1], x[2] | y[2])
+    return 0 if (x | y == 1) else 1
 
 def logical_xor(x, y):
-    return (x[0] ^ y[0], x[1] ^ y[1], x[2] ^ y[2])
+    return 0 if (x ^ y == 1) else 1
 
 def logical_not(x):
-    return (255 - x[0], 255 - x[1], 255 - x[2])
+    return 0 if (x == 0) else 1
+
+def logical_nand(x, y):
+    return 0 if logical_not(logical_and(x, y)) else 1
+
+def logical_nor(x, y):
+    return 0 if logical_not(logical_or(x, y)) else 1
+
+def logical_xnor(x, y):
+    return 0 if logical_not(logical_xor(x, y)) else 1
+
+
+
